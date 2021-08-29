6a91af03875dcd5908eccca7513110b1b6c16cfd
Jed <jedtony.jt@gmail.com>
2021-08-29 01:45:10 +0100

    Test extract

--- src/route/route.js
+++ src/route/route.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
           name: "Jed Tony",
           github: "@jeddtony",
           email: "jed@gmail.com",
-          mobile: "test build fail",
+          mobile: "test extract",
           twitter: "@main",
         },
       });
