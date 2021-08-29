6b2d334d0cd166fa5ff49ce546d364fd8b637cac
Jed <jedtony.jt@gmail.com>
2021-08-29 02:00:53 +0100

    Test sudo

--- src/route/route.js
+++ src/route/route.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
           name: "Jed Tony",
           github: "@jeddtony",
           email: "jed@gmail.com",
-          mobile: "test sudo privileges",
+          mobile: "test free permissions",
           twitter: "@main",
         },
       });
